b22129a1f4e2c9775ecd9aaf366d14490fd61a80
Wade Wilson <wadewilson@216.200.96.25.ipyx-116884-zyo.zip.zayo.com>
2017-02-05 02:18:42 -0800

    update

--- Card Shuffler/main.cpp
+++ Card Shuffler/main.cpp
@@ -38,18 +38,14 @@ void deck_shuffle(vector<card>* pdeck){    // shuffles the deck
 
 
 void showdeck(vector<card>* pdeck){   // function for outputting deck to console
-    //int num_suits = 52;
-    //int num_numbers = 1;
     for(int i=0; i<pdeck->size(); i++){
-        //for(int j=0; j<num_numbers; j++){
             cout << "Index: " << i << " Suit: "<< pdeck->at(i).suit << " Num: "<< pdeck->at(i).number << endl;
-        //}
     }
 }
 
 void testA(vector<card>* pdeck){
     if(pdeck->size()>53){
-        cout << "Test A Will Fail with More than one deck, skipping test A" << endl;
+        cout << "Test A will fail with more than one deck, skipping test A" << endl;
     }
     else {
     card A;
@@ -91,7 +87,7 @@ void testA(vector<card>* pdeck){
 
 void testB(vector<card>* pdeck){
     if(pdeck->size()<53){
-        cout << "Test B Will Fail only one deck, skipping test B" << endl;
+        cout << "Test B will fail with only one deck, skipping test B" << endl;
     }
     else {
         card A;
@@ -105,7 +101,6 @@ void testB(vector<card>* pdeck){
                 
                 bool suitissame = false;
                 bool numissame = false;
-                bool isidenticle = false;
                 
                 if(A.suit == B.suit){
                     suitissame = true;
@@ -134,7 +129,7 @@ int main() {
     int num_decks = 1;          // Number of Decks
     int num_suits = 4;
     int num_numbers = 13;
-    for( int N=0; N<num_decks; N++){
+    for(int N=0; N<num_decks; N++){
     for(int i=0; i<num_suits; i++){     // creating deck
         for(int j=0; j<num_numbers; j++){
             card temp_c;
@@ -168,4 +163,5 @@ int main() {
         cout << "Output File Error" << endl;
     }
     outputFile.close();
+    cout << "Shuffled deck outputted to deckout.txt" << endl;
 }
